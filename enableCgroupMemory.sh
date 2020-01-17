6cd8308af9aa7c9161a71c4ab5e1235e6d222a8f
Zhuoli Liang <zhuoliseattle@gmail.com>
2020-01-16 23:49:08 -0800

    Update enableCgroupMemory.sh

--- enableCgroupMemory.sh
+++ enableCgroupMemory.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sed -i '$ s/$/ cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory' /boot/firmware/nobtcmd.txt
+sed -i '$ s/$/ cgroup_enable=cpuset cgroup_memory=1 cgroup_enable=memory/' /boot/firmware/nobtcmd.txt
